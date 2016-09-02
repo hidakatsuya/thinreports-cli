990704c7c56fcbe695e2dd4187ff7c6acf23db5b
Katsuya HIDAKA <hidakatsuya@gmail.com>
2016-09-03 01:28:52 +0900

    Rename: Commands to Application

--- lib/thinreports/cli.rb
+++ lib/thinreports/cli.rb
@@ -1,4 +1,4 @@
 require 'thinreports'
 
 require_relative 'cli/version'
-require_relative 'cli/commands'
+require_relative 'cli/application'
--- lib/thinreports/cli/application.rb
+++ lib/thinreports/cli/application.rb
@@ -3,7 +3,7 @@ require_relative 'command/upgrade'
 
 module Thinreports
   module Cli
-    class Commands < Thor
+    class Application < Thor
       desc 'upgrade', 'Upgrade .tlf to 0.9.x from 0.8.x.'
       def upgrade(source, destination)
         Command::Upgrade.new(source, destination).call
